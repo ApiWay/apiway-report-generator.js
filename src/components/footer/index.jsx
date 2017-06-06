0b170f2db3781a47cf3321d1439d7ba51a704478
Youngbok Yoon <master@bluehack.net>
2017-06-06 16:00:24 +0900

    [APIWAY-140] assets path를 AWS s3로 public하게 수정

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -7,7 +7,7 @@ const cx = classNames.bind(styles);
 
 const urls = {
   site: 'http://adamgruber.github.io/mochawesome/',
-  github: 'https://github.com/adamgruber'
+  github: 'https://github.com/apiway
 };
 
 const Footer = () => {
@@ -16,9 +16,9 @@ const Footer = () => {
     <footer className={ cx('component') }>
       <div className='container'>
         <p>
-          &copy;{ copyrightYear }&nbsp;<a href={ urls.site } target='_blank' rel='noopener noreferrer'>Mochawesome</a>
+          &copy;{ copyrightYear }&nbsp;<a href={ urls.site } target='_blank' rel='noopener noreferrer'>apiway-report</a>
           &nbsp;was designed and built by&nbsp;
-          <a href={ urls.github } target='_blank' rel='noopener noreferrer'>Adam Gruber</a>
+          <a href={ urls.github } target='_blank' rel='noopener noreferrer'>ApiWay</a>
         </p>
       </div>
     </footer>
